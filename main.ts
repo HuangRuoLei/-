a2296a71f0782625a7f493722d8b8341b795d8de
HuangRuoLei <69152711+HuangRuoLei@users.noreply.github.com>
2021-06-01 15:13:58 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -64,24 +64,31 @@ namespace HuLuMaoexpanding {
     export function Key(index:key_number): boolean {
         
         let temp: boolean = false;
-        if (index <= 3) {
-            pins.digitalWritePin(DigitalPin.P14, 0)
-            pins.digitalWritePin(DigitalPin.P15, 0)
-            pins.digitalWritePin(DigitalPin.P16, 1)
-        } else if ((4 <= index) && (index <= 7)) {
-            pins.digitalWritePin(DigitalPin.P14, 0)
-            pins.digitalWritePin(DigitalPin.P15, 1)
-            pins.digitalWritePin(DigitalPin.P16, 0)
-        } else if (8 <= index) {
-            pins.digitalWritePin(DigitalPin.P14, 1)
-            pins.digitalWritePin(DigitalPin.P15, 0)
-            pins.digitalWritePin(DigitalPin.P16, 0)
-        }
+        // if (index <= 3) {
+        //     pins.digitalWritePin(DigitalPin.P14, 0)
+        //     pins.digitalWritePin(DigitalPin.P15, 0)
+        //     pins.digitalWritePin(DigitalPin.P16, 1)
+        // } else if ((4 <= index) && (index <= 7)) {
+        //     pins.digitalWritePin(DigitalPin.P14, 0)
+        //     pins.digitalWritePin(DigitalPin.P15, 1)
+        //     pins.digitalWritePin(DigitalPin.P16, 0)
+        // } else if (8 <= index) {
+        //     pins.digitalWritePin(DigitalPin.P14, 1)
+        //     pins.digitalWritePin(DigitalPin.P15, 0)
+        //     pins.digitalWritePin(DigitalPin.P16, 0)
+        // }
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
         switch (index) {
-            case key_number._0 | key_number._4 | key_number._8: if (pins.digitalReadPin(DigitalPin.P8) == 1) { temp = true } break;
-            case key_number._1 | key_number._5 | key_number._9: if (pins.digitalReadPin(DigitalPin.P9) == 1) { temp = true } break;
-            case key_number._2 | key_number._6 | key_number._F1: if (pins.digitalReadPin(DigitalPin.P12) == 1) { temp = true } break;
-            case key_number._3 | key_number._7 | key_number._F2: if (pins.digitalReadPin(DigitalPin.P13) == 1) { temp = true } break;
+            case key_number._0 : if (pins.digitalReadPin(DigitalPin.P8) == 1) { temp = true } break;
+            case key_number._1 : if (pins.digitalReadPin(DigitalPin.P9) == 1) { temp = true } break;
+            case key_number._2 : if (pins.digitalReadPin(DigitalPin.P12) == 1) { temp = true } break;
+            case key_number._3 : if (pins.digitalReadPin(DigitalPin.P13) == 1) { temp = true } break;
+            // case key_number._0 | key_number._4 | key_number._8: if (pins.digitalReadPin(DigitalPin.P8) == 1) { temp = true } break;
+            // case key_number._1 | key_number._5 | key_number._9: if (pins.digitalReadPin(DigitalPin.P9) == 1) { temp = true } break;
+            // case key_number._2 | key_number._6 | key_number._F1: if (pins.digitalReadPin(DigitalPin.P12) == 1) { temp = true } break;
+            // case key_number._3 | key_number._7 | key_number._F2: if (pins.digitalReadPin(DigitalPin.P13) == 1) { temp = true } break;
         }
         return temp;
     }
